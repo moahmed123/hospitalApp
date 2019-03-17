88fbd287621364959015343f5feec7409e45cbbf
moahmed123 <mohamed_ll14@yahoo.com>
2019-03-17 22:24:41 +0200

    add ads google

--- dist/container/AppBody/List.js
+++ dist/container/AppBody/List.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import { Icon, Card, CardItem, View, Body, Text } from 'native-base';
 
 class List extends Component {
-    
+
   render() {
-    return (      
+    return (
         <Card>
             <CardItem>
                 <Body style={{ textAlign: 'right', direction: "rtl", width: "100%",flexDirection:'row', flexWrap:'wrap'}}>
-                    <Text style={{ 
-                        textAlign: 'right', direction: "rtl", width: "100%", 
+                    <Text style={{
+                        textAlign: 'right', direction: "rtl", width: "100%",
                         fontWeight: "600", color: "#3498db",fontSize: 18
                         }}>
                         {this.props.AllData['name']}
-                    </Text>                    
+                    </Text>
                     <View style={{ display: 'flex' }}>
                         <Text style={{
-                            width: null, lineHeight: 25, 
+                            width: null, lineHeight: 25,
                             textAlign: 'right', direction: "rtl",
                             marginBottom: 10, paddingTop: 10, color: '#34495e',
                         }}>
@@ -24,34 +24,34 @@ class List extends Component {
                         </Text>
                         <View style={{position: 'absolute', right: 0, top: 12}}>
                             <Icon name='location' type='Entypo' style={{color: '#34495e', fontSize: 20}}/>
-                        </View>    
+                        </View>
                     </View>
                     {/* Three Box Color Background Data */}
-                    <Text style={{ 
+                    <Text style={{
                         textAlign: 'right', direction: "rtl",
-                        backgroundColor: "#3498db", color: "#fff", 
+                        backgroundColor: "#3498db", color: "#fff",
                         paddingVertical: 5, paddingHorizontal:10,
                         borderRadius: 5, marginRight: 5, marginTop: 5,fontSize: 14
                         }}>
                         {this.props.AllData['categories']}
-                    </Text>                    
-                    <Text style={{ 
-                        textAlign: 'right', direction: "rtl", 
-                        backgroundColor:"#34495e", color: "#fff", 
+                    </Text>
+                    <Text style={{
+                        textAlign: 'right', direction: "rtl",
+                        backgroundColor:"#34495e", color: "#fff",
                         paddingVertical: 5, paddingHorizontal:10,
                         borderRadius: 5,marginRight: 5, marginTop: 5
                         }}>
                         <Icon type="FontAwesome" name="phone" style={{color: '#fff',fontSize: 15}}/>
-                        <Text style={{color: '#fff', fontSize: 14}}> {this.props.AllData['phone']}</Text>                        
+                        <Text style={{color: '#fff', fontSize: 14}}> {this.props.AllData['phone']}</Text>
                     </Text>
-                    <Text style={{ 
+                    <Text style={{
                         textAlign: 'right', direction: "rtl",
                         backgroundColor:"#9b59b6", color: "#fff",
                         paddingVertical: 5, paddingHorizontal:10, width: null,
                         borderRadius: 5, marginTop: 5, fontSize: 14
                         }}>
                         {this.props.AllData['city']}
-                    </Text>                    
+                    </Text>
                     {/* Three Box Color Background Data */}
                 </Body>
             </CardItem>
@@ -59,4 +59,4 @@ class List extends Component {
     );
   }
 }
-export default List;        
+export default List;
--- dist/container/AppBody/Result.js
+++ dist/container/AppBody/Result.js
@@ -5,14 +5,15 @@ import * as actionCreatores from '../../actions';
 import { Container, Spinner, Content, Icon, Fab} from 'native-base';
 import AppHeader from "../AppHeader";
 import List from "./List";
+import {AdMobBanner} from 'react-native-admob-dfp'
 
-const {height} = Dimensions.get('window');    
+const {height} = Dimensions.get('window');
 
 class Result extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            refreshing: false,            
+            refreshing: false,
             latitude  : "",
             longitude : "",
             limit     : "",
@@ -20,16 +21,16 @@ class Result extends Component {
             dis       : "",
             name      : "",
             city      : "",
-            cat       : "", 
-            active    : 'true'            
+            cat       : "",
+            active    : 'true'
         };
         this._onRefresh = this._onRefresh.bind(this);
-    }    
+    }
     static navigationOptions = {
         header: null
     };
     // Use When Refresh Data.
-    _onRefresh(){        
+    _onRefresh(){
         this.setState({refreshing: true});
         this.props.GetHospitalData(
             this.state.name,
@@ -43,68 +44,75 @@ class Result extends Component {
             true
             ).then(() => {
           this.setState({refreshing: false});
-          
+
         });
     }
     componentDidMount() {
-        BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);          
-        {this._requestLocationPermission()}              
+        BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
+        {this._requestLocationPermission()}
     }
     componentWillUnmount() {
         BackHandler.removeEventListener('hardwareBackPress', this.handleBackPress);
     }
     handleBackPress = () => {
-        //BackHandler.exitApp(); // To Exit App.         
-        
+        //BackHandler.exitApp(); // To Exit App.
+
         alert('backToLogo   => '+ this.props.navigation.state.routeName)
     }
     onClickFun() {
         this.props.navigation.navigate('filter');
         console.log('ada')
     }
-    // Loop For Data 
-    _ShowAllData(){               
-        if (!this.props.AllData) {            
-            return (                
+    // Loop For Data
+    _ShowAllData(){
+        if (!this.props.AllData) {
+            return (
                 <View style = {{flex: 1,height: height-135}}>
-                    <Spinner color="#16a085" style={{paddingTop: (height-135)/2, textAlign: "center"}}/> 
-                </View>                    
+                    <Spinner color="#16a085" style={{paddingTop: (height-135)/2, textAlign: "center"}}/>
+                </View>
             );
         }else{
-            console.log(this.props.AllData['count']); 
+            console.log(this.props.AllData['count']);
             const CountData =  this.props.AllData['count'];
             if(CountData <= 0 ){
-                return (                    
-                    <View style = {{flex: 1,height: height-135}}>
+                return (
+                    <View style = {{flex: 1,height: height-133}}>
                         <Icon type='FontAwesome5' name='hospital-alt' style={{
                             paddingTop: (height-200)/2, textAlign: "center",
                             fontSize: 45, color: '#16a085'
                         }}/>
-                        <Text style = {{textAlign: "center", color: '#2c3e50', fontSize: 16, paddingTop:15}}>                        
-                            No 
-                            <Text style={{fontWeight: '600', color:'#16a085', fontSize: 18}} > Hospitals </Text> 
+                        <Text style = {{textAlign: "center", color: '#2c3e50', fontSize: 16, paddingTop:15}}>
+                            No
+                            <Text style={{fontWeight: '600', color:'#16a085', fontSize: 18}} > Hospitals </Text>
                             Found In This Area
-                        </Text>                                                                            
-                    </View>                    
-                );  
+                        </Text>
+                        <View style={{justifyContent: 'flex-end', flex:1 }}>
+                            <AdMobBanner
+                                adSize="fullBanner"
+                                adUnitID="ca-app-pub-7316325922246137/9456673457"
+                                testDevices={[AdMobBanner.simulatorId]}
+                            />
+                        </View>
+                    </View>
+                );
             }else{
                 return this.props.AllData["hospital"].map((data, i)=>{
                     return(
-                        <List key = {i} AllData = {data}/>            
+                        <List key = {i} AllData = {data}/>
                     );
                 });
             }
         }
-        
+
     }
-    // Fun Access Location 
-    _requestLocationPermission(){                                           
+    // Fun Access Location
+    _requestLocationPermission(){
         return new Promise(async() => {
             const permissions = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
             if (permissions === PermissionsAndroid.RESULTS.GRANTED){
                 // Alert.alert('access true');
                 navigator.geolocation.getCurrentPosition(
-                    (position) => {                      
+                    (position) => {
                       const latitude  = position.coords.latitude,
                             longitude = position.coords.longitude,
                             limit     = 20,
@@ -123,19 +131,21 @@ class Result extends Component {
                                 city      : city,
                                 cat       : cat
                             })
-                      this.props.GetHospitalData(name,city,cat,limit,minlimit,longitude,latitude,dis,true);                      
+                      this.props.GetHospitalData(name,city,cat,limit,minlimit,longitude,latitude,dis,true);
                     },
-                    (error) => {alert(error.message)}                    
-                ); 
+                    (error) => {alert(error.message)}
+                );
             }
             else{
                 BackHandler.exitApp();
             }
         });
     }
-    // Function Show Seach Button 
+    // Function Show Seach Button
     ShowSearchButton(){
-        if(this.props.AllData){
+        if(this.props.AllData ){
+          const CountDataSearch =  this.props.AllData['count'];
+          if(CountDataSearch > 0){
             return(
                 <Fab
                     active={this.state.active}
@@ -145,18 +155,20 @@ class Result extends Component {
                     position="bottomRight"
                     onPress={() => this.props.navigation.navigate('Search')}>
                         <Icon name="ios-search" type='Ionicons' style={{fontSize:25}}/>
-                </Fab>                           
+                </Fab>
             )
+          }
+
         }
     }
-    render() {            
+    render() {
         return (
             <Container>
                 <AppHeader Navigation={this.props.navigation} />
                 <Content style={{ textAlign: 'right', direction: "rtl"}}
                 refreshControl={
-                        <RefreshControl 
-                            refreshing={this.state.refreshing} 
+                        <RefreshControl
+                            refreshing={this.state.refreshing}
                             onRefresh={this._onRefresh}
                             title="Pull to refresh"
                             tintColor="#9b59b6"
@@ -165,8 +177,20 @@ class Result extends Component {
                             />
                     }
                 >
-                    {this._ShowAllData()}                     
-                </Content>                          
+                    {this._ShowAllData()}
+                    <View style={{paddingTop: 10}}>
+                          {/*}<AdMobBanner
+                              adSize="banner"
+                              adUnitID="ca-app-pub-3940256099942544/2934735716"
+                              testDevices={[AdMobBanner.simulatorId]}
+                          />*/}
+                          <AdMobBanner
+                              adSize="banner"
+                              adUnitID="ca-app-pub-7316325922246137/9456673457"
+                              testDevices={[AdMobBanner.simulatorId]}
+                          />
+                    </View>
+                </Content>
                 {this.ShowSearchButton()}
             </Container>
         );
